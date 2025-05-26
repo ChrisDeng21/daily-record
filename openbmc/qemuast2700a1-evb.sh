cf47bb0be681d9012ebd09fe2092684a1141580e
ChrisDeng21 <99303039+ChrisDeng21@users.noreply.github.com>
2025-05-26 17:09:05 +0800

    Create qemuast2700a1-evb.sh

---
+++ openbmc/qemuast2700a1-evb.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+# [ Base Settings ]
+
+# The QEMU tool path depends on your system,
+# feel free to adjust it to match your system configuration.
+QEMUTOOL=/home/chrisdeng/Work/Project/qemu/build/qemu-system-aarch64
+
+# The build path does not require modification,
+# but you can change it if it does not meet your system.
+BUILD_PATH=build/*/tmp/deploy/images/*
+
+# The port settings does not require modification,
+# but you can change it if it does not meet your system.
+SSH_PORT=2222
+HTTPS_PORT=2443
+IPMI_PORT=2623
+
+
+# [ Check Environments ]
+
+if [[ ! $(which $QEMUTOOL) ]]; then
+    echo "Error! QEMU tool not installed."
+    exit 1
+fi
+
+if [ -d $BUILD_PATH ]; then
+    BUILD_DIR=`dirname $BUILD_PATH`
+    MACHINE=`basename $BUILD_PATH`
+else
+    echo "Error! cann't find build folder."
+    exit 1
+fi
+
+while netstat -tuln | grep ":$SSH_PORT " > /dev/null 2>&1; do
+    echo "SSH Port $SSH_PORT is in use, trying next..."
+    SSH_PORT=$((SSH_PORT + 1))
+done
+
+while netstat -tuln | grep ":$HTTPS_PORT " > /dev/null 2>&1; do
+    echo "HTTPS Port $HTTPS_PORT is in use, trying next..."
+    HTTPS_PORT=$((HTTPS_PORT + 1))
+done
+
+while netstat -tuln | grep ":$IPMI_PORT " > /dev/null 2>&1; do
+    echo "IPMI Port $IPMI_PORT is in use, trying next..."
+    IPMI_PORT=$((IPMI_PORT + 1))
+done
+
+# [ Set Image Variables ]
+
+# Refer https://www.qemu.org/docs/master/system/arm/aspeed.html#booting-the-ast2700-evb-machine
+IMGDIR=$BUILD_DIR/$MACHINE
+UBOOT_SIZE=$(stat --format=%s -L $IMGDIR/u-boot-nodtb.bin)
+COMMAND="$QEMUTOOL -M ast2700a1-evb \
+  -device loader,force-raw=on,addr=0x400000000,file=$IMGDIR/u-boot-nodtb.bin \
+  -device loader,force-raw=on,addr=$((0x400000000 + $UBOOT_SIZE)),file=$IMGDIR/u-boot.dtb \
+  -device loader,force-raw=on,addr=0x430000000,file=$IMGDIR/bl31.bin \
+  -device loader,force-raw=on,addr=0x430080000,file=$IMGDIR/optee/tee-raw.bin \
+  -device loader,cpu-num=0,addr=0x430000000 \
+  -device loader,cpu-num=1,addr=0x430000000 \
+  -device loader,cpu-num=2,addr=0x430000000 \
+  -device loader,cpu-num=3,addr=0x430000000 \
+  -smp 4 \
+  -drive file=$IMGDIR/image-bmc,format=raw,if=mtd \
+  -nographic \
+  -net nic \
+  -net user,hostfwd=:127.0.0.1:$SSH_PORT-:22,hostfwd=:127.0.0.1:$HTTPS_PORT-:443,hostfwd=udp:127.0.0.1:$IPMI_PORT-:623,hostname=qemu
+"
+
+echo "================================================================================"
+echo BUILD_DIR=$BUILD_DIR
+echo MACHINE=$MACHINE
+echo IMGDIR=$IMGDIR
+echo UBOOT_SIZE=$UBOOT_SIZE
+echo SSH_PORT=$SSH_PORT
+echo HTTPS_PORT=$HTTPS_PORT
+echo IPMI_PORT=$IPMI_PORT
+echo COMMAND=$COMMAND
+echo "================================================================================"
+
+
+# [ Play ]
+
+exec $COMMAND
